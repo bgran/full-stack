89fd1065005650354d4497b244f76c22c9feab7f
Bo Granlund <bo.granlund@helsinki.fi>
2021-02-07 14:22:34 +0200

    Tehtävä 2.5

--- osa2/kurssitiedot/App.js
+++ osa2/kurssitiedot/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Course from './Course'
 
 const Header = (props) => {
         return (
@@ -7,52 +8,6 @@ const Header = (props) => {
        )
 }
 
-const Part = (props) => {
-        return (
-                <div>
-                        {props.name}: {props.harkkoja}
-                </div>
-        )
-}
-
-const Content = (props) => {
-
-        return (
-                <div>
-                        {props.course["parts"].map((item, index) => (
-                                <Part name={item["name"]} harkkoja={item["exercises"]} key={index} />
-                        ))}
-                </div>
-        )
-}
-
-const Total = (props) => {
-	const ind = props.ind
-	const o = props.course_data
-
-	const o2 = o
-	const w_tot = o2["parts"].reduce((a, b) => {
-		return a + b["exercises"]
-	}, 0 )
-
-        return (
-                <div>
-                        <b>total of {w_tot} exercises</b>
-                </div>
-        )
-}
-
-const Course = (props) => {
-	let o = props.course
-	let i = props.ind
-	return (
-		<div>
-			<h2>{o["name"]}</h2>
-			<Content course={o} />
-			<Total course_data={o} ind={i} />
-		</div>
-	)
-}
 const Courses = (props) => {
 	let o = props.courses
 	return (
@@ -71,7 +26,7 @@ const App = () => {
                 	"name": "Half Stack application development",
 			id:1,
                 	"parts":[
-                        	{"name": "Fundamentals of React", "exercises":10, id:1},
+                        	{"name": "Fundamentals of Reach", "exercises":10, id:1},
                         	{"name": "Using props to pass data", "exercises":7, id:2},
                         	{"name": "State of component", "exercises":14, id:3},
 				{"name": "Redux", "exercises":11, id:4}
@@ -86,14 +41,6 @@ const App = () => {
 			]
 		}
 	]
-
-
-        //const part1 = 'Reactin perusteet'
-        //const exercises1 = 10
-        //const part2 = 'Tiedonvälitys propseilla'
-        //const exercises2 = 7
-        //const part3 = 'Komponenttien tila'
-        //const exercises3 = 14
 
         return (
                 <div>
