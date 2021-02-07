1ac87be1afd224a6dbaf96b78e8adff764cc916a
Bo Granlund <bo.granlund@helsinki.fi>
2021-02-07 23:23:49 +0200

    Tehtävä 2.11

--- osa2/puhelinluettelo/App.js
+++ osa2/puhelinluettelo/App.js
@@ -1,18 +1,54 @@
-import React, { Component, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import  UserForm from './UserForm'
+import UserForm from './UserForm'
 import Name from './Name'
 import Filter from "./Filter"
+import axios from 'axios'
 
 const App = (props) => {
 	//console.log("PROPSIT: ", props)
+
+	let huoh = []
+	//const state_init = JSON.parse(props.persons_data)
 	const [ persons, setPersons ] = useState([
-		{name: 'Arto Hellas', key:1, number: "666-1234-1234" }
+		//{name: 'Arto Hellas', key:1, number: "666-1234-1234" }
 	])
+
 	const [ newName, setNewName ] = useState('')
 	const [ newNumber, setNewNumber ] = useState('')
 	const [ searchName, setSearchName ] = useState('')
+
+
+	let stop = false
+	const [isStarted, setIsStarted] = useState(0);
+
+	useEffect(() => {
+		console.log("in useEffect")
+		const eventHandler = response => {
+			console.log("promise fulfilled")
+			setPersons(response.data)
+		}
+		const promise = axios.get("http://localhost:3001/persons")
+		promise.then(eventHandler)
+	}, [])
+
+	const addPersons = (person_data) => {
+		console.log("addPersons: ", person_data)
+		for (let i=0; i<person_data.length; i++) {
+			const k = person_data[i]
+			console.log("k", k)
+			const nameObj = {
+				name: k.name,
+				number: k.number,
+				key: i
+			}
+			console.log("lisättavä: ", nameObj)
+			setPersons(persons.concat(nameObj))
+
+			console.log("setPersons: ", persons.length)
+		}
+	}
 
 	const addName = (event) => {
 		event.preventDefault()
@@ -31,7 +67,8 @@ const App = (props) => {
 
 		const nameObj = {
 			name: newName,
-			number: newNumber
+			number: newNumber,
+			key: persons.length
 		}
 		setPersons(persons.concat(nameObj))
 		setNewName('')
@@ -58,17 +95,16 @@ const App = (props) => {
 		setSearchName(needle)
 	}
 
+	console.log("persons: lopussa  ", persons)
+
 	return (
 		<div>
 			<h2>Puhelinluettelo</h2>
-			<p>
 			<form>
 				<div>
 					<Filter persons={persons} searchName={searchName} searchFunc={handleSearchName} />
 				</div>
 			</form>
-			</p>
-			<p>
 				<div>
 					<UserForm persons={persons} form1={handleNameChange}
 						form2={handleNumberChange}
@@ -79,8 +115,7 @@ const App = (props) => {
 						name_callback={addName}
 					/>
 				</div>
-			</p>
-			<h2>Numbers</h2>
+			<h2>Numerot</h2>
 			<div>
 				<Name perso={persons} currsearch={searchName} searchFunc={handleSearchName} />
 			</div>
